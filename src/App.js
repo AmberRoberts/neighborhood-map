eed0f40e9d95faa80a7558598a0b634cfc01161f
Amber Roberts <amberrobertsvt@gmail.com>
2018-09-27 21:18:44 -0400

    Foursquare is set up and markers dynamically populate

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 // import MapContainer from './MapContainer'
 // import Map from './Map'
 // import { GoogleApiWrapper } from 'google-maps-react'
-// Currently Referencing: https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/
-
+// Reference https://www.fullstackreact.com/articles/how-to-write-a-google-maps-react-component/
+// https://stackoverflow.com/questions/45429484/how-to-implement-google-maps-js-api-in-react-without-an-external-library
 // or this tutorial https://github.com/fullstackreact/google-maps-react
 // reference here https://stackoverflow.com/questions/48493960/using-google-map-in-react-component
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
+
   componentDidMount() {
-    this.loadMap()
+    this.getVenues()
   }
 
   loadMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyA_f2pEa5Dk54Q0uWavdCQcSkJSZDvLx8g&callback=initMap")
     window.initMap = this.initMap
   }
+/* via tutorial on creating a foursquare API call by Elharony Published on YouTube, Aug 17, 2018, as well as via foursquare documentation */
+
+  getVenues = () => {
+    const endpoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "VEJCMS5O4IFLMWFHQNX5XNJL5TEGZSQ5LZGMJYJZ23CFBGKV",
+      client_secret: "Q1CYMWTSVHPNBDMNBQAZ1EXTCROV31TMGQZ3MTGZWVUPPCSX",
+      query: "",
+      near: "Hagnau am Bodensee",
+      v: 20180927 // API version - meaning app is prepared for API changes up to this date
+    }
+
+    axios.get(endpoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({ venues: response.data.response.groups[0].items }, this.loadMap())
+      console.log(response.data.response.groups[0].items) // creates an array of objects as generated in the above API call
+    })
+    .catch(error => {
+      console.log("Whoops, something went wrong" + error)
+    })
+  }
 
   initMap = () => {
           let map = new window.google.maps.Map(document.getElementById('map'), {
             center: {lat: 47.6753, lng: 9.3185},
-            zoom: 12
+            zoom: 17
           });
+
+          this.state.venues.map(venueMarker => {
+            var marker = new window.google.maps.Marker({
+              position: {lat: venueMarker.venue.location.lat, lng: venueMarker.venue.location.lng},
+              map: map,
+              title: venueMarker.venue.name
+          })
+        });
         }
 
 
@@ -42,7 +77,7 @@ class App extends Component {
 function loadScript(url) {
   let index = window.document.getElementsByTagName("script")[0]
 
-  /* create a script tag like the API requires, using VanillaJS
+  /* create a script tag as required by the API, using VanillaJS to generate this from documentation:
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
   async defer></script> */
 
